e3be262131787f95dff53e6292d5bdb0e6642242
Timofey <timashajz@yandex.ru>
2021-11-05 17:11:22 +0300

    fix

--- internal/api/discordConnect/realTimeServer.go
+++ internal/api/discordConnect/realTimeServer.go
@@ -14,7 +14,7 @@ func Broadcast(messege []byte) {
 	for i := 0; i < len(utils.ConnSlice); i++ {
 		err := utils.ConnSlice[i].WriteMessage(websocket.TextMessage, messege)
 		if err != nil {
-			go close(i)
+			close(i)
 			utils.ConnSlice = append(utils.ConnSlice[:i], utils.ConnSlice[i+1:]...)
 		}
 	}
