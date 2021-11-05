8be19284428f2f73997333bccbb53e015f089e2e
Timofey <timashajz@yandex.ru>
2021-11-05 19:47:42 +0300

    fix

--- internal/api/discordConnect/realTimeServer.go
+++ internal/api/discordConnect/realTimeServer.go
@@ -6,17 +6,15 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-func close(conn *websocket.Conn, message []byte) {
+func close(conn *websocket.Conn) {
 	conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(1000, "woops"))
 	conn.Close()
 }
-func Broadcast(messege []byte, wsSlice []*websocket.Conn) {
-	var internal []*websocket.Conn
-	internal = append(internal, wsSlice...)
-	for i := 0; i < len(internal); i++ {
-		err := internal[i].WriteMessage(websocket.TextMessage, messege)
+func Broadcast(messege []byte) {
+	for i := 0; i < len(utils.ConnSlice); i++ {
+		err := utils.ConnSlice[i].WriteMessage(websocket.TextMessage, messege)
 		if err != nil {
-			utils.ConnSlice[i].Close()
+			close(utils.ConnSlice[i])
 			utils.ConnSlice = append(utils.ConnSlice[:i], utils.ConnSlice[:i+1]...)
 		}
 	}
